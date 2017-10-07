01a3f428cd027e6857e4a9da3f602e714eb53333
OscarJMatosCanul <13470202@itcampeche.edu.mx>
2017-10-06 22:56:56 -0500

    IMC: Primer version funcional del proyecto. Incluye manejo de eventos.

--- app/src/main/java/com/oscarmatos/android/imc/MainActivity.java
+++ app/src/main/java/com/oscarmatos/android/imc/MainActivity.java
@@ -2,12 +2,50 @@ package com.oscarmatos.android.imc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mCampoPeso;
+    EditText mCampoEstatura;
+    Button mBotonCalcular;
+    Button mBotonLimpiar;
+    TextView mEtiquetaIMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCampoPeso=(EditText)findViewById(R.id.campo_peso);
+        mCampoEstatura=(EditText)findViewById(R.id.campo_estatura);
+        mBotonCalcular=(Button)findViewById(R.id.boton_calcular);
+        mBotonLimpiar=(Button)findViewById(R.id.boton_limpiar);
+        mEtiquetaIMC=(TextView)findViewById(R.id.etiqueta_imc);
+
+        mBotonCalcular.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String s = mCampoPeso.getText().toString();
+                double peso=Double.parseDouble(s);
+                s=mCampoEstatura.getText().toString();
+                double estatura=Double.parseDouble(s);
+                double imc = peso/(estatura*estatura);
+                s=String.format("%2.2f",imc);
+                mEtiquetaIMC.setText(s);
+            }
+        });
+
+        mBotonLimpiar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCampoPeso.setText("");
+                mCampoEstatura.setText("");
+                mEtiquetaIMC.setText("0.0");
+            }
+        });
     }
 }
